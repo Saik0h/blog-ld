bd18537d01074c4c32062960e4ec83457b7b72c4
Saik0h <omcristhyan@gmail.com>
2025-05-13 00:47:27 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    pathMatch: 'full',
+    loadComponent:()=> {
+        return import('./components/homepage/homepage.component').then((m)=> m.HomepageComponent)
+    }},{
+    path:'login',
+    loadComponent:()=>{
+        return import('./components/login/login.component').then((m)=> m.LoginComponent)
+    }},{
+    path:'register',
+    pathMatch: 'full',
+    loadComponent:()=> {
+        return import('./components/register/register.component').then((m)=> m.RegisterComponent)
+    }},{
+    path:'contact',
+    loadComponent:()=>{
+        return import('./components/contact/contact.component').then((m)=> m.ContactComponent)
+    }},{
+    path:'about',
+    pathMatch: 'full',
+    loadComponent:()=> {
+        return import('./components/about/about.component').then((m)=> m.AboutComponent)
+    }},{
+    path:'blogs',
+    loadComponent:()=>{
+        return import('./components/blogs/blogs.component').then((m)=> m.BlogsComponent)
+    },
+}];
