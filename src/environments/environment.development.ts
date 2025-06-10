b24081f405d5a1d05a990c8e7ab10a15ed5ac69f
Saik0h <omcristhyan@gmail.com>
2025-06-10 09:50:16 -0300

    updated favicon and minor changes to styles

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    supabaseUrl:'https://ggygefogxhkvhceujppc.supabase.co',
-    supabaseKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdneWdlZm9neGhrdmhjZXVqcHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTYzMTMsImV4cCI6MjA2MTA5MjMxM30.nw_l8jz3FXZG6svC-P72BZQZLvoPh9Cz4aHNYri_Qyg'
-};
